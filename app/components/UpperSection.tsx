02730530d6e4a61739a8bf9454edf81abb366985
Senin Adın <senin_email_adresin@example.com>
2025-04-30 20:00:44 +0100

    content changed

--- app/components/UpperSection.tsx
+++ app/components/UpperSection.tsx
@@ -3,15 +3,15 @@ import React from 'react'
 const UpperSection = () => {
   return (
     <div className="bg-gradient-to-b from-[#3737376e] to-[#3C3C3C6e] rounded-[40px] p-6">
-      <h2 className="text-white text-center text-xl mb-8">Overview</h2>
+      <h2 className="text-white text-center text-xl mb-8">Analysis of Last 7 day</h2>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-        {['Proteins 180g', 'Calories 4328', 'Activity Time 1.5 hour'].map((title, i) => (
+        {['Avarage Proteins Per Day: 180g', 'Avarage Calories Per Day: 4328', 'KGs Gained: 0.83 KG'].map((title, i) => (
           <div key={i} className="bg-gradient-to-b from-[#3737376e] to-[#3C3C3C6e] rounded-[40px] p-6">
             <div className="h-48 bg-gradient-to-b from-[#8979FF4D] to-[#2627231A] rounded-xl mb-4"/>
             <h3 className="text-white text-lg mb-3">{title}</h3>
             <div className="text-[#767585] space-y-2">
-              <div className="flex justify-between"><span>Goal</span><span>8000</span></div>
-              <div className="flex justify-between"><span>Average</span><span>9500</span></div>
+              <div className="flex justify-between"><span className='text-[#c7cccc]'>Goal</span><span className='text-[#c7cccc]'>13 Kg</span></div>
+              <div className="flex justify-between"><span className='text-[#c7cccc]'>Weekly Average</span><span className='text-[#c7cccc]'>1 Kg</span></div>
             </div>
           </div>
         ))}
--- app/dashboard.tsx
+++ app/dashboard.tsx
@@ -8,8 +8,8 @@ import Calendar from './components/Calendar'
 
 const Dashboard = () => {
   return (
-    <div className="min-h-screen bg-[#1A1A1A] p-6">
-      <div className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-[auto_1fr_auto] gap-6">
+    <div className="min-h-screen bg-gradient-to-br from-blue-200 to-[#86828260] p-6">
+      <div className="max-w-9xl mx-auto grid grid-cols-1 lg:grid-cols-[auto_1fr_auto] gap-6">
         {/* Left Sidebar */}
         <LeftSidebar />
 
