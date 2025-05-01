ee551d279aeafee6375a64f97c2765c386a39217
Senin Adın <senin_email_adresin@example.com>
2025-05-01 10:57:08 +0100

    new folder created for homepage and its components

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Dashboard from "./dashboard";
-import HomePage from "./homepage";
+import HomePage from "./homepage/homepage";
 
 export default function Home() {
   return (
